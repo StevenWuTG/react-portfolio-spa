efb8eea06e47157e4bd621f16f370564ebbc8a69
StevenWuTG <stevenwu91@icloud.com>
2021-06-20 01:49:01 -0400

    fixed scss dependencies

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './components/Navbar'
+import Intro from './components/Intro'
+import Portfolio from './components/Portfolio'
+import Contact from './components/Contact'
+// import './App.css';
+import './app.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="app">
+      
+      <Navbar/>
+      <div className="sections">
+        <Intro/>
+        <Portfolio/>
+        <Contact/>
+      </div>
+
     </div>
   );
 }
